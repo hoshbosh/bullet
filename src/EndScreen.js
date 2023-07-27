38a936699ad1c7f1722567e588768c0c742092ab
Joshua Labasbas <90983893+hoshbosh@users.noreply.github.com>
2023-07-27 10:29:02 -0400

    Update EndScreen.js

--- src/EndScreen.js
+++ src/EndScreen.js
@@ -2,7 +2,7 @@ import './App.css'
 export function EndScreen(props){
     return(
         <div className="absolute inset-0 bg-red-800 text-center font-sans text-5xl p-28 text-slate-100">
-            FUCK YOU
+            GAME OVERR
             <h1>
                 {props.winner}
             </h1>
